e94824b2aed77b183abbb2b6360c746fe8208e3b
yugmani <yugmani@yahoo.com>
2022-08-04 11:32:16 -0700

    conditional rendering with jsx by if ... else, element variables, ternary operator and short circuit methods

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import UserGreeting from './components/UserGreeting';
 
 export default function App() {
   return (
     <div>
-  
+      <UserGreeting />
     </div>
   );
 }
---
+++ src/components/UserGreeting.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+
+class UserGreeting extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoggedIn: false,
+    };
+
+    // this.state.isLoggedIn = this.state.isLoggedIn.bind(this);
+  }
+
+  render() {
+    // Using if ... else block
+    // *************************
+    // if (this.state.isLoggedIn) {
+    //   return (
+    //     <div>
+    //       <h1>Conditional Rendering</h1>
+    //       <div className="logged">Welcome Yoog</div>
+    //     </div>
+    //   );
+    // } else {
+    //   return (
+    //     <div>
+    //       <h1>Conditional Rendering</h1>
+    //       <div className="not-logged">Welcome Guest</div>
+    //     </div>
+    //   );
+    // }
+
+    // NOTE:
+    // we cannot include if ... else statement iside return block because jsx is just a syntactic sugar for function calls and object construction.
+
+    // using Element Variables
+    // *************************
+    // let message;
+    // if (this.state.isLoggedIn) {
+    //   message = <div className="logged">Welcome Yoog</div>;
+    // } else {
+    //   message = <div className="not-logged">Welcome Guest</div>;
+    // }
+
+    // return (
+    //   <div>
+    //     <h1>Conditional Rendering</h1>
+    //     {message}
+    //   </div>
+    // );
+
+    // Using Ternary Operator
+    // *************************
+    return (
+      <div>
+        <h1>Conditional Rendering</h1>
+        {this.state.isLoggedIn ? (
+          <div className="logged">Welcome Yoog</div>
+        ) : (
+          <div className="not-logged">Welcome Guest</div>
+        )}
+      </div>
+    );
+
+    // Using Short Circuit method
+    // *************************
+    // return (
+    //   <div>
+    //     <h1>Conditional Rendering</h1>
+    //     {this.state.isLoggedIn && <div className="logged">Welcome Yoog</div>}
+    //   </div>
+    // );
+  }
+}
+
+export default UserGreeting;
