fabd43cb5f07cdfcc9ca9cce0f17e2975f9ec132
yugmani <yugmani@yahoo.com>
2022-08-04 12:44:25 -0700

    Problems of using index as a key

--- src/components/Person.js
+++ src/components/Person.js
@@ -10,3 +10,10 @@ function Person({ person }) {
 }
 
 export default Person;
+
+//when to use index as a key?
+// ***************************
+
+// 1. The items in your list do not have a unique id.
+// 2. The list is a static list and will not change.
+// 3. The list will never be reordered or filtered.
