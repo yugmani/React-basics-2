fc16744a00b9efccec0088c4281d515f2b29e5a5
yugmani <yugmani@yahoo.com>
2022-08-06 23:48:22 -0700

    Memo

--- src/components/ParentComp.js
+++ src/components/ParentComp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import RegularComp from './RegularComp';
-import PureComp from './PureComp';
+// import RegularComp from './RegularComp';
+// import PureComp from './PureComp';
+import MemoComp from './MemoComp';
 
 class ParentComp extends Component {
   constructor(props) {
@@ -12,19 +13,20 @@ class ParentComp extends Component {
 
   //uncomment below to see the difference in rendering of pure component and regular components.
   componentDidMount() {
-    // setInterval(() => {
-    //   this.setState({
-    //     name: 'Yoog',
-    //   });
-    // }, 10000);
+    setInterval(() => {
+      this.setState({
+        name: 'Yoog',
+      });
+    }, 2000);
   }
   render() {
     console.log('********** Parent Component **************');
     return (
       <div>
         Parent Component
-        <RegularComp name={this.state.name} />
-        <PureComp name={this.state.name} />
+        <MemoComp name={this.state.name} />
+        {/* <RegularComp name={this.state.name} />
+        <PureComp name={this.state.name} /> */}
       </div>
     );
   }
